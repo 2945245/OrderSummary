a7353a77050256808803bd0cb0bd5f4af8768be7
2945245 <87025282+2945245@users.noreply.github.com>
2021-11-16 10:03:40 +0100

    Initial commit

---
+++ OrderSummary/Controllers/InfoOrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderSummary.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfoOrderController :ControllerBase
+    {
+        
+    }
+}
